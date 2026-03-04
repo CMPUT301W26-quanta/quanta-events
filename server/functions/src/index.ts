4f8ead9d7779d554714e9039d5d02a137a02e2cb
Divy-21 <divyvaghasiya21@gmail.com>
2026-03-04 08:09:50 -0700

    functions added: getUser and createUser (before pull)

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -8,8 +8,10 @@
  */
 
 import { setGlobalOptions } from "firebase-functions";
-import { onCall } from "firebase-functions/https";
+import { onCall, HttpsError } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+import * as z from "zod";
+import { v4 as uuidv4 } from 'uuid';
 
 import { initializeApp } from "firebase-admin/app";
 // import { getFirestore } from 'firebase-admin/firestore'
@@ -37,4 +39,67 @@ export const health = onCall({ maxInstances: 1 }, async (_request) => {
   return {
     time: now,
   };
+});
+
+
+const createUserInterface = z.object({
+  deviceId : z.string(),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  phone: z.number().int().optional(),
+  receiveNotifications: z.boolean().optional(),
+  isEntrant: z.boolean().optional(),
+  isOrganizer: z.boolean().optional(),
+  isAdmin: z.boolean().optional(),
+});
+
+export const createUser = onCall({ maxInstances: 1 }, async (_request) => {
+  const result = createUserInterface.safeParse(_request.data);
+  if (!result.success) {
+    throw new HttpsError('invalid-argument', 'Missing Required Fields');
+  }
+
+  const { deviceId, name, email, phone, receiveNotifications, isEntrant, isOrganizer, isAdmin } = result.data;
+
+  const userId = uuidv4();
+
+  const db = getFirestore();
+  await db.collection('users').doc(userId).set({
+    deviceId,        
+    ...(name && { name }),
+    ...(email && { email }),
+    ...(phone && { phone }),
+    entrant: isEntrant ? { enteredEvents: [], receiveNotifications: receiveNotifications ?? false } : null,
+    organizer: isOrganizer ? { createdEvents: [], sentNotifications: [] } : null,
+    admin: isAdmin ? {} : null,
+  });
+
+  logger.info('Created user', { userId });
+
+  return {
+    userId
+  };
+});
+
+
+const getUserSchema = z.object({
+  userId: z.string().uuid(),
+});
+
+export const getUser = onCall({ maxInstances: 1}, async (_request) => {
+  const result = getUserSchema.safeParse(_request.data);
+  if (!result.success) {
+    throw new HttpsError("invalid-argument", "Missing Required Fields");
+  }
+
+  const {userId} = result.data;
+  const db = getFirestore();
+  const userDoc = await db.collection('users').doc(userId).get();
+
+  if (!userDoc.exists) {
+      throw new HttpsError('not-found', 'User not found');
+  }
+
+  logger.info('User found', { userId });
+  return userDoc.data();
 });
