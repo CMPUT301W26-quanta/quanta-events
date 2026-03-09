bc27805fbcdf2fc1c5db654424aa519b1b0e984d
komalpr1 <107446211+komalpr1@users.noreply.github.com>
2026-03-08 20:16:29 -0600

    Make info fragment (#61)
    * Made icon fragment and button
    
    Made fragment for info page and button
    
    * Updated info fragment
    
    Added nav and button to info fragment
    
    * Updated information fragment
    
    I have updated the back button on info fragment to be correct color, and changed text color to correct color as well.

--- src/app/src/main/java/ca/quanta/quantaevents/fragments/InformationFragment.java
+++ src/app/src/main/java/ca/quanta/quantaevents/fragments/InformationFragment.java
@@ -12,34 +12,46 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
+import java.util.zip.Inflater;
+
 import ca.quanta.quantaevents.R;
-import ca.quanta.quantaevents.databinding.FragmentTestOneBinding;
+import ca.quanta.quantaevents.databinding.FragmentInformationBinding;
 import ca.quanta.quantaevents.stores.FragmentInfoStore;
 
-public class TestFragmentOne extends Fragment {
-    private FragmentTestOneBinding binding;
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InformationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InformationFragment extends Fragment {
 
-    public TestFragmentOne() {
+    public FragmentInformationBinding binding;
+
+
+    public InformationFragment() {
         // Required empty public constructor
     }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InformationFragment.
+     */
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         FragmentInfoStore infoStore = new ViewModelProvider(requireActivity()).get(FragmentInfoStore.class);
-        infoStore.setTitle("Home");
-        infoStore.setSubtitle("Receive lottery selection updates here.");
-        infoStore.setIconRes(R.drawable.material_symbols_home_outline);
+        infoStore.setTitle("Information");
+        infoStore.setSubtitle("Lottery selection information here.");
+        infoStore.setIconRes(R.drawable.material_symbols_info_outline);
 
-        binding.accountButton.setOnClickListener(_view -> {
-            NavDirections action = TestFragmentOneDirections.actionTestFragmentOneToTestFragmentTwo();
-            Navigation.findNavController(binding.getRoot()).navigate(action);
-        });
-
-
-        binding.infoButton.setOnClickListener(_view -> {
-            NavDirections action = TestFragmentOneDirections.actionTestFragmentOneToInformationFragment();
+        binding.backButton.setOnClickListener(_view -> {
+            NavDirections action = InformationFragmentDirections.actionGlobalTestFragmentOne();
             Navigation.findNavController(binding.getRoot()).navigate(action);
         });
     }
@@ -47,7 +59,7 @@ public class TestFragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentTestOneBinding.inflate(inflater, container, false);
+        binding = FragmentInformationBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 }
