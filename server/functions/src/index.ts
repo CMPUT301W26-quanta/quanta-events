ff80201522cc2bd6b41c056fabbd19a7b444e43e
Divy-21 <100563505+Divy-21@users.noreply.github.com>
2026-03-09 12:25:50 -0600

    Fixed deployment (#63)
    * Add functions: get, create Event and Image and fixes
    
    * Changed onCall to onRequest and added auth checks
    
    * Switched to onCall() to make functions compatible with Firebase SDK
    
    * Some fixes
    
    * Build fail fix
    
    ---------
    
    Co-authored-by: Jed Krause <122229354+JorunnaParva06@users.noreply.github.com>

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -41,6 +41,30 @@ export const health = onCall({ maxInstances: 1 }, async (_request) => {
   };
 });
 
+type Role = "entrant" | "admin" |"organizer";
+
+async function verifyUser(userId: string, deviceId: string) {
+  const db = getFirestore();
+
+  const userDoc = await db.collection("users").doc(userId).get();
+
+  if (!userDoc.exists) {
+    throw new HttpsError("not-found", "User does not exist");
+  }
+
+  const data = userDoc.data()!;
+
+  if (data.deviceId !== deviceId) {
+    throw new HttpsError("unauthenticated", "Device ID does not match");
+  }
+  return data;
+}
+
+async function requireRole(userData: any, role: Role) {
+  if (!userData[role] || userData[role] === null) {
+    throw new HttpsError("permission-denied", `User is not an ${role}`);
+  }
+}
 
 const authInterface = z.object({
   userId: z.string().uuid,
