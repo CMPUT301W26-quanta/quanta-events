28ad4b73cf2ff5143db69df7f5e8acdc07fe2408
Talyn Veugelers <talyn.veugelers@gmail.com>
2026-03-10 13:29:26 -0600

    Add smart burger groups (#67)

--- src/app/src/main/java/ca/quanta/quantaevents/burger/SmartBurgerState.java
+++ src/app/src/main/java/ca/quanta/quantaevents/burger/SmartBurgerState.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 public class SmartBurgerState extends ViewModel {
     private UUID active;
+    private int groupFilter;
     private final MutableLiveData<Boolean> display;
 
     SmartBurgerState() {
@@ -19,8 +20,39 @@ public class SmartBurgerState extends ViewModel {
         return active;
     }
 
+    int getGroupFilter() {
+        return groupFilter;
+    }
+
     LiveData<Boolean> getDisplay() {
         return display;
+    }
+
+    /**
+     * Allows a given bitmask of groups to be displayed in the {@link SmartBurger}
+     *
+     * @param group A bitmask combination of {@link SmartBurger#ENTRANT_GROUP}, {@link SmartBurger#ORGANIZER_GROUP}, and {@link SmartBurger#ADMIN_GROUP}
+     */
+    public void allowGroup(int group) {
+        this.groupFilter |= group;
+    }
+
+    /**
+     * Disallows a given bitmask of groups from being displayed in the {@link SmartBurger}
+     *
+     * @param group A bitmask combination of {@link SmartBurger#ENTRANT_GROUP}, {@link SmartBurger#ORGANIZER_GROUP}, and {@link SmartBurger#ADMIN_GROUP}
+     */
+    public void disallowGroup(int group) {
+        this.groupFilter &= ~group;
+    }
+
+    /**
+     * Sets a given bitmask of groups to be displayed in the {@link SmartBurger}
+     *
+     * @param group A bitmask combination of {@link SmartBurger#ENTRANT_GROUP}, {@link SmartBurger#ORGANIZER_GROUP}, and {@link SmartBurger#ADMIN_GROUP}
+     */
+    public void setGroupFilter(int group) {
+        this.groupFilter = group;
     }
 
     public <F extends Fragment & Tagged> void show(F fragment) {
