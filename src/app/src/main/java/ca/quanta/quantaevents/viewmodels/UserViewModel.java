6efab65293478d627a4155afbdad2ea6598b7ffb
Jed Krause <122229354+JorunnaParva06@users.noreply.github.com>
2026-03-11 13:39:57 -0600

    Connecting UI to data (#69)
    * Added registration UI
    
    * Connecting the register fragment to firestore. I added a viewmodel class, which is the one that calls the cloud function. The fragment has this viewmodel. Also some changes to the user model, changing phone number from an int to a string.
    
    * getUser function
    
    * getUser function
    
    * Make the getUser function return a user object
    
    * Change the createUser function to return a UUID object instead of a string that represents a UUID
    
    ---------
    
    Co-authored-by: Divy-21 <divyvaghasiya21@gmail.com>

--- src/app/src/main/java/ca/quanta/quantaevents/viewmodels/UserViewModel.java
+++ src/app/src/main/java/ca/quanta/quantaevents/viewmodels/UserViewModel.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import ca.quanta.quantaevents.models.User;
+
 /**
  * Class which represents a UserViewModel object.
  */
@@ -32,7 +34,7 @@ public class UserViewModel extends ViewModel {
      * @param deviceId UUID identifying the user's device.
      * @return UUID identifying the user's ID.
      */
-    public Task<String> createUser(String name, String email, String phone, Boolean isEntrant, Boolean isOrganizer, Boolean isAdmin, Boolean getNotifications, UUID deviceId) {
+    public Task<UUID> createUser(String name, String email, String phone, Boolean isEntrant, Boolean isOrganizer, Boolean isAdmin, Boolean getNotifications, UUID deviceId) {
         // Arguments for createUser
         Map<String, Object> data = new HashMap<>();
         data.put("deviceId", deviceId.toString());
@@ -47,12 +49,43 @@ public class UserViewModel extends ViewModel {
         return functions
                 .getHttpsCallable("createUser")
                 .call(data)
-                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                .continueWith(new Continuation<HttpsCallableResult, UUID>() {
                     // Access return value of the function
                     @Override
-                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                    public UUID then(@NonNull Task<HttpsCallableResult> task) throws Exception {
                         Map<String, Object> result = (Map<String, Object>) task.getResult().getData();
-                        return (String) result.get("userId");
+                        return UUID.fromString((String) result.get("userId"));
+                    }
+                });
+    }
+
+    /**
+     * Calls the getUser cloud function, getting a user's details from the database.
+     * @param userId UUID identifying the user.
+     * @param deviceId UUID identifying the user's device.
+     * @return User object with the user's information.
+     */
+    public Task<User> getUser(UUID userId, UUID deviceId) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("userId", userId.toString());
+        data.put("deviceId", deviceId.toString());
+
+        return functions
+                .getHttpsCallable("getUser")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, User>() {
+                    @Override
+                    public User then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        Map<String, Object> userData = (Map<String, Object>) task.getResult().getData();
+                        String name = (String) userData.get("name");
+                        String email = (String) userData.get("email");
+                        String phoneNumber = (String) userData.get("phone");
+                        Boolean receiveNotifications = (Boolean) userData.get("receiveNotifications");
+                        Boolean isEntrant = (Boolean) userData.get("isEntrant");
+                        Boolean isOrganizer = (Boolean) userData.get("isOrganizer");
+                        Boolean isAdmin = (Boolean) userData.get("isAdmin");
+                        User result = new User(name, email, phoneNumber, receiveNotifications, isEntrant, isOrganizer, isAdmin, userId, deviceId);
+                        return result;
                     }
                 });
     }
