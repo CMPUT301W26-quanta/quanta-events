caa9f6526ad7b196e11a871f7ce915fad893ce2a
JorunnaParva06 <jedwua9@gmail.com>
2026-03-12 15:28:17 -0600

    Work in progress update for createNotification, worked on getting the specific users I wanna send a notification to

--- server/functions/src/functions/createNotification.ts
+++ server/functions/src/functions/createNotification.ts
@@ -3,14 +3,18 @@ import { CallableRequest, HttpsError } from "firebase-functions/https";
 import * as z from "zod";
 import * as util from "../util";
 import { v4 as uuidv4 } from "uuid";
-import { getFirestore } from "firebase-admin/firestore";
+import { CollectionReference, getFirestore } from "firebase-admin/firestore";
+import { EventDocument } from "../schema";
 
 const createNotificationInterface = util.standardForm(
   z.object({
     senderId: z.uuid(),
-    recipients: z.enum(["wonLottery", "lostLottery", "waiting", "cancelled", "selectedFromWaitlist"]).nullable(),
     message: z.string().optional(),
     title: z.string().optional(),
+    eventId: z.uuid(),
+    waited: z.boolean(),
+    cancelled: z.boolean(),
+    selected: z.boolean(),
   })
 );
 
@@ -22,13 +26,13 @@ export async function createNotification(request: CallableRequest) {
       request
     );
   
-    const { senderId, recipients, message, title } = data;
+    const { senderId, message, title, eventId, waited, cancelled, selected } = data;
   
     const userData = await util.verifyUser(userId, deviceId);
     await util.requireRole(userData, "organizer");
   
     const notificationId = uuidv4();
-  
+    
     const db = getFirestore();
   
     try {
@@ -37,14 +41,50 @@ export async function createNotification(request: CallableRequest) {
       .doc(notificationId)
       .create({
         senderId,
-        recipients,
         message,
         title,
+        eventId,
+        waited,
+        cancelled,
+        selected,
       });
     } catch (_) {
       throw new HttpsError("already-exists", "Notification already exists");
     }
-  
-    logger.info("Notification Created", { notificationId });
+
+    const eventDocuments = db.collection("events") as CollectionReference<EventDocument, EventDocument>;
+    const events = (await eventDocuments.doc(eventId).get()).data();
+
+    // Store all the recipients in some collection
+    const recipients: string[] = [];
+    if (waited) {
+      const waitedList = events?.waitList as string[];
+      for (const entrantId of waitedList) {
+        if (!recipients.includes(entrantId)) {
+          recipients.push(entrantId);
+        }
+      }
+    }
+    if (cancelled) {
+      const cancelledList = events?.cancelledList as string[];
+      for (const entrantId of cancelledList) {
+        if (!recipients.includes(entrantId)) {
+          recipients.push(entrantId);
+        }
+      }
+    }
+    if (selected) {
+      const finalList = events?.finalList as string[];
+      for (const entrantId of finalList) {
+        if (!recipients.includes(entrantId)) {
+          recipients.push(entrantId);
+        }
+      }
+    }
+
+    // TODO get the FCM codes by querying the user objects in the recipient list
+    // TODO Send notification to the recipients
+
+    logger.info("Notification Created and sent succesfully", { notificationId });
     return { notificationId };
 }
