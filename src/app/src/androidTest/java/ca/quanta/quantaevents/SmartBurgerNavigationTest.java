cf06267dfb8c02a0e81fb254327daf4121914b96
Komal Kaur <komalpr1@ualberta.ca>
2026-03-12 17:18:26 -0600

    Implemented waiting time and register screen to test
    Buttons were being clicked before they had oppoutunity to display. So allowed for wait time between view pop up and clicking, and now will skip registration to go to home screen

--- src/app/src/androidTest/java/ca/quanta/quantaevents/SmartBurgerNavigationTest.java
+++ src/app/src/androidTest/java/ca/quanta/quantaevents/SmartBurgerNavigationTest.java
@@ -3,35 +3,79 @@ package ca.quanta.quantaevents;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import static org.hamcrest.CoreMatchers.allOf;
+
+import android.view.View;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 
+import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ca.quanta.quantaevents.fragments.HomeFragment;
+
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SmartBurgerNavigationTest {
 
+    @Before
+    public void skipRegistration() {
+        try {
+            waitForView(withId(R.id.save_button));
+            onView(withId(R.id.save_button)).perform(click());
+        } catch (Exception e) {
+
+        }
+    }
 
 
-    @Rule
+
+@Rule
     public ActivityScenarioRule<MainActivity> activityRule = new
             ActivityScenarioRule<>(MainActivity.class);
 
+
+    private void waitForView(Matcher<View> matcher){
+
+        long end = System.currentTimeMillis() + 5000;
+        while (System.currentTimeMillis() < end){
+            try {
+                onView(matcher).check(matches(isDisplayed()));
+                return;
+            } catch (Throwable e) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {}
+            }
+        }
+        onView(matcher).check(matches(isDisplayed()));
+
+    }
+
     private void openBurgerMenu(){
-        onView(allOf(withContentDescription("Open Menu"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
+        Matcher<View> burgerButton = allOf(withContentDescription("Open Menu"), isDescendantOfA(withId(R.id.coordinator)));
+
+        waitForView(burgerButton);
+        onView(burgerButton).perform(click());
+    }
+
+    private void clickMenuItem(String label){
+        Matcher<View> item = allOf(withContentDescription(label), isDescendantOfA(withId(R.id.coordinator)));
+
+        waitForView(item);
+        onView(item).perform(click());
     }
 
 
@@ -45,8 +89,7 @@ public class SmartBurgerNavigationTest {
     public void testBurgerMenuHidesCurrentPage_Home(){
 
         openBurgerMenu();
-        onView(allOf(withContentDescription("Home"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
-
+        clickMenuItem("Home");
         openBurgerMenu();
 
         onView(withContentDescription("Home")).check(doesNotExist());
@@ -59,8 +102,7 @@ public class SmartBurgerNavigationTest {
     public void testBurgerMenuHidesCurrentPage_AdminPanel(){
 
         openBurgerMenu();
-        onView(allOf(withContentDescription("Admin Panel"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
-
+        clickMenuItem("Admin Label");
         openBurgerMenu();
 
         onView(withContentDescription("Admin Panel")).check(doesNotExist());
@@ -70,7 +112,7 @@ public class SmartBurgerNavigationTest {
     public void testBurgerMenuHidesCurrentPage_Dashboard(){
 
         openBurgerMenu();
-        onView(allOf(withContentDescription("Dashboard"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
+        clickMenuItem("Dashboard");
 
         openBurgerMenu();
 
@@ -80,7 +122,7 @@ public class SmartBurgerNavigationTest {
     public void testBurgerMenuHidesCurrentPage_EventList(){
 
         openBurgerMenu();
-        onView(allOf(withContentDescription("Event List"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
+        clickMenuItem("Event List");
 
         openBurgerMenu();
 
@@ -91,13 +133,10 @@ public class SmartBurgerNavigationTest {
     public void testBurgerMenuHidesCurrentPage_Information(){
 
         openBurgerMenu();
-
-        onView(allOf(withContentDescription("Home"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
+        clickMenuItem("Home");
 
         openBurgerMenu();
-
-        onView(allOf(withContentDescription("Information"), isDescendantOfA(withId(R.id.coordinator)))).perform(click());
-
+        clickMenuItem("Information");
         openBurgerMenu();
 
         onView(withContentDescription("Information")).check(doesNotExist());
