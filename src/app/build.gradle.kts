41ed35fed9ba0bdf4b274cf81f7457f46e1f5695
GlitchlessCode <talyn.veugelers@gmail.com>
2026-03-13 14:25:04 -0600

    Add user unit test

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -40,6 +40,10 @@ android {
     }
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 dependencies {
 
     implementation(libs.appcompat)
@@ -56,9 +60,13 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
     implementation("com.google.firebase:firebase-functions")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
     implementation(libs.zxing.android.embedded)
 }
---
+++ src/app/src/test/java/ca/quanta/quantaevents/UserUnitTest.java
@@ -0,0 +1,109 @@
+package ca.quanta.quantaevents;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import ca.quanta.quantaevents.models.User;
+
+
+public class UserUnitTest {
+    private static String NAME = "TestUser";
+    private static String EMAIL = "test.user@gmail.com";
+    private static String PHONE = "1234567890";
+
+    @Test
+    public void UserBasicCreationTest() {
+        User user = new User(NAME, EMAIL, PHONE, true, true, true, true);
+
+        assertEquals(user.getName(), NAME, "Failed with name");
+        assertEquals(user.getEmail(), EMAIL, "Failed with email");
+        assertEquals(user.getPhoneNumber(), PHONE, "Failed with phone");
+        assertTrue(user.isEntrant());
+        assertTrue(user.isOrganizer());
+        assertTrue(user.isAdmin());
+        assertNotNull(user.getEntrant());
+        assertTrue(user.getEntrant().getReceiveNotifications());
+
+        user = new User(NAME, EMAIL, PHONE, true, false, true, true);
+
+        assertFalse(user.isEntrant());
+        assertTrue(user.isOrganizer());
+        assertTrue(user.isAdmin());
+        assertNull(user.getEntrant());
+
+        user = new User(NAME, EMAIL, PHONE, true, true, false, true);
+
+        assertTrue(user.isEntrant());
+        assertFalse(user.isOrganizer());
+        assertTrue(user.isAdmin());
+        assertNotNull(user.getEntrant());
+
+        user = new User(NAME, EMAIL, PHONE, true, true, true, false);
+
+        assertTrue(user.isEntrant());
+        assertTrue(user.isOrganizer());
+        assertFalse(user.isAdmin());
+        assertNotNull(user.getEntrant());
+    }
+
+    @Test
+    public void UserServerCreationTest() {
+        UUID userId = UUID.randomUUID();
+        UUID deviceId = UUID.randomUUID();
+
+        User user = new User(null, null, null, false, false, false, false, userId, deviceId);
+        HashMap<String, Object> data = new HashMap<>();
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with blank data");
+
+        data.put("unrelated", 1234);
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with unrelated data");
+
+        data.put("name", NAME);
+        data.put("email", EMAIL);
+        data.put("phone", PHONE);
+
+        user = new User(NAME, EMAIL, PHONE, false, false, false, false, userId, deviceId);
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with simple data");
+
+        HashMap<String, Object> entrant = new HashMap<>();
+        entrant.put("receiveNotifications", false);
+        data.put("entrant", entrant);
+
+        user = new User(NAME, EMAIL, PHONE, false, true, false, false, userId, deviceId);
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with entrant, no notifications");
+
+        entrant.put("receiveNotifications", true);
+
+        user = new User(NAME, EMAIL, PHONE, true, true, false, false, userId, deviceId);
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with entrant, yes notifications");
+
+        data.put("organizer", new HashMap<>());
+        data.put("admin", new HashMap<>());
+
+        user = new User(NAME, EMAIL, PHONE, true, true, true, true, userId, deviceId);
+
+        assertEquals(user, new User(data, userId, deviceId), "Failed with full set");
+    }
+
+    @Test
+    public void UserIDTest() {
+        UUID userId = UUID.randomUUID();
+        UUID deviceId = UUID.randomUUID();
+        User user = new User(NAME, EMAIL, PHONE, false, false, false, false, userId, deviceId);
+        assertEquals(user.getUserId(), userId, "Failed with userId");
+        assertEquals(user.getDeviceId(), deviceId, "Failed with deviceId");
+    }
+}
