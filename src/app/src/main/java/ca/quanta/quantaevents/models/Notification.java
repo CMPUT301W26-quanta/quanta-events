34ddbca0c61f76cbd664426fd263d5a9bbedd8dc
resoinaut <resoinaut@gmail.com>
2026-03-26 16:19:59 -0600

    create getAllNotifications cloud function; make use of it

--- src/app/src/main/java/ca/quanta/quantaevents/models/Notification.java
+++ src/app/src/main/java/ca/quanta/quantaevents/models/Notification.java
@@ -1,30 +1,38 @@
 package ca.quanta.quantaevents.models;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 /**
  * Class which defines a Notification.
  */
 public class Notification {
-    private final UUID notificationId;
-    private UUID senderId;
-    private ArrayList<UUID> recipientIds;
+    private UUID eventId;
     private String title;
     private String message;
+    private Boolean waited;
+    private Boolean selected;
+    private Boolean cancelled;
 
     /**
      * Constructor for a Notification object.
-     * @param senderId ID of a User who sent the notification.
-     * @param recipientIds The IDs of users whom the notification was sent to.
+     * @param eventId The id of the event this notification is associated with.
+     * @param title The notification's title.
      * @param message The notification's message.
+     * @param waited If sent to waitlisted users.
+     * @param selected If sent to selected users.
+     * @param cancelled If sent to cancelled users.
      */
-    public Notification(UUID senderId, ArrayList<UUID> recipientIds, String title, String message) {
-        this.notificationId = UUID.randomUUID();
-        this.senderId = senderId;
-        this.recipientIds = recipientIds;
-        this.message = message;
+    public Notification(UUID eventId, String title, String message, Boolean waited, Boolean selected, Boolean cancelled) {
+        this.eventId = eventId;
         this.title = title;
+        this.message = message;
+        this.waited = waited;
+        this.selected = selected;
+        this.cancelled = cancelled;
+    }
+
+    public UUID getEventId() {
+        return this.eventId;
     }
 
     public String getTitle() {
@@ -33,5 +41,17 @@ public class Notification {
 
     public String getMessage() {
         return this.message;
+    }
+
+    public Boolean getSentToWaitlisted() {
+        return this.waited;
+    }
+
+    public Boolean getSentToSelected() {
+        return this.selected;
+    }
+
+    public Boolean getSentToCancelled() {
+        return this.cancelled;
     }
 }
--- src/app/src/main/java/ca/quanta/quantaevents/viewmodels/NotificationViewModel.java
+++ src/app/src/main/java/ca/quanta/quantaevents/viewmodels/NotificationViewModel.java
@@ -8,16 +8,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.functions.FirebaseFunctions;
 import com.google.firebase.functions.HttpsCallableResult;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import ca.quanta.quantaevents.models.Notification;
 
 /**
  * View-model for managing notification-related data and cloud functions.
  */
 public class NotificationViewModel extends ViewModel {
-
     private FirebaseFunctions functions = FirebaseFunctions.getInstance();
+
+    public NotificationViewModel() {
+        functions.useEmulator("10.0.2.2", 5001);
+    }
 
     /**
      * Calls the createNotification cloud function, creating and sending a notification.
@@ -59,4 +66,39 @@ public class NotificationViewModel extends ViewModel {
                 });
     }
 
+    public Task<ArrayList<Notification>> getAllNotifications(UUID userId, UUID deviceId, UUID sentById) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("userId", userId.toString());
+        data.put("deviceId", deviceId.toString());
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("sentById", sentById);
+        data.put("data", payload);
+
+        return functions
+                .getHttpsCallable("getAllNotifications")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, ArrayList<Notification>>() {
+                    @Override
+                    public ArrayList<Notification> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        List<Map<String, Object>> notificationObjects = (List<Map<String, Object>>) task.getResult().getData();
+                        ArrayList<Notification> notifications = new ArrayList<>();
+
+                        for (Map<String, Object> notificationObject : notificationObjects) {
+                            UUID eventId = UUID.fromString((String) notificationObject.get("eventId"));
+
+                            String title = (String) notificationObject.get("title");
+                            String message = (String) notificationObject.get("message");
+
+                            Boolean waited = (Boolean) notificationObject.get("waited");
+                            Boolean selected = (Boolean) notificationObject.get("selected");
+                            Boolean cancelled = (Boolean) notificationObject.get("cancelled");
+
+                            notifications.add(new Notification(eventId, title, message, waited, selected, cancelled));
+                        }
+
+                        return notifications;
+                    }
+                });
+    }
 }
