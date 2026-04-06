af4e7b525d86741e5bc19693c9f2fbc10e1cb467
Komal Kaur <komalpr1@ualberta.ca>
2026-04-06 11:35:37 -0600

    CommentsUITesting
    Completed comments UI testing

--- src/app/src/androidTest/java/ca/quanta/quantaevents/CommentsUITesting.java
+++ src/app/src/androidTest/java/ca/quanta/quantaevents/CommentsUITesting.java
@@ -18,9 +18,14 @@ import static org.hamcrest.CoreMatchers.allOf;
 import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static org.hamcrest.CoreMatchers.not;
+
 
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -39,38 +44,59 @@ public class CommentsUITesting {
     @Before
     public void navigateToEventDetails(){
 
-        Matcher<View> burgerButton = allOf(withContentDescription("Open Menu"), isDescendantOfA(withId(R.id.coordinator)));
+        try {
 
-        onView(burgerButton).perform(click());
+            onView(withId(R.id.input_name)).perform(typeText("Test User"), closeSoftKeyboard());
+
+            String email = "test" + System.currentTimeMillis() + "@mail.com";
+
+            onView(withId(R.id.input_email)).perform(typeText(email), closeSoftKeyboard());
+
+            onView(withId(R.id.save_button)).perform(click());
+
+            onView(withId(R.id.coordinator)).check(matches(isDisplayed()));
+        } catch (Exception ignored) {
+
+        }
+
+        openBurgerMenu();
+
         clickMenuItem("Event List");
 
-        onView(withId(R.id.events_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
+        waitForView(withId(R.id.search_button));
+
+        onView(withId(R.id.search_button)).perform(click());
+
+        waitForView(withId(R.id.events_recycler_view));
+
+        onView(withId(R.id.events_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
     }
 
     @Test
     public void commentInputIsDisplayed(){
-        onView(withId(R.id.comment_text)).check(matches(isDisplayed()));
+        Matcher<View> commentInput = allOf(withId(R.id.type_comment), not(isDescendantOfA(withId(R.id.comments_recycler_view))));
+        onView(commentInput).check(matches(isDisplayed()));
 
     }
 
     @Test
     public void typingCommentShowsTextInInput(){
-        onView(withId(R.id.comment_text)).perform(typeText("This is a test comment"), closeSoftKeyboard());
-
-        onView(withId(R.id.comment_text)).check(matches(withText("This is a test comment")));
-
+        Matcher<View> commentInput = allOf(withId(R.id.type_comment), not(isDescendantOfA(withId(R.id.comments_recycler_view))));
+        onView(commentInput).perform(scrollTo(), typeText("This is a test comment"), closeSoftKeyboard());
+        onView(commentInput).check(matches(withText("This is a test comment")));
     }
 
     @Test
     public void postingCommentAppearsInList(){
         String testMessage = "Hello this is a test comment";
 
-        onView(withId(R.id.comment_text)).perform(typeText(testMessage), closeSoftKeyboard());
+        onView(withId(R.id.type_comment)).perform(typeText(testMessage), closeSoftKeyboard());
 
         onView(withId(R.id.send_comment)).perform(click());
 
+        waitForView(withId(R.id.comments_recycler_view));
         onView(withId(R.id.comments_recycler_view)).check(matches(hasDescendant(withText(testMessage))));
 
     }
@@ -83,16 +109,46 @@ public class CommentsUITesting {
 
     @Test
     public void emptyCommentDoesNotPost(){
+
+        Matcher<View> commentInput = allOf(withId(R.id.type_comment), not(isDescendantOfA(withId(R.id.comments_recycler_view))));
+
         onView(withId(R.id.send_comment)).perform(click());
 
-        onView(withId(R.id.comment_text)).check(matches(withText("")));
+        onView(commentInput).check(matches(withText("")));
     }
+
 
 
     private void clickMenuItem(String label){
         Matcher<View> item = allOf(withContentDescription(label), isDescendantOfA(withId(R.id.coordinator)));
 
         onView(item).perform(click());
+    }
+
+
+
+    private void waitForView(Matcher<View> matcher){
+
+        long end = System.currentTimeMillis() + 7000;
+        while (System.currentTimeMillis() < end){
+            try {
+                onView(matcher).check(matches(isDisplayed()));
+                return;
+            } catch (Throwable e) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {}
+            }
+        }
+        onView(matcher).check(matches(isDisplayed()));
+
+    }
+
+    private void openBurgerMenu(){
+        Matcher<View> burgerButton = allOf(withContentDescription("Open Menu"), isDescendantOfA(withId(R.id.coordinator)));
+
+        waitForView(burgerButton);
+        onView(burgerButton).perform(click());
     }
 
 
